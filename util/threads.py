a770ad80fc6e561353596ee0bece0c597b1543b4
Matthias Schreiber <mat@boar.bar>
2016-09-12 18:16:08 +0200

    added deamon=True to thread decorator

--- util/threads.py
+++ util/threads.py
@@ -5,5 +5,7 @@ import threading
 
 def thread(func):
     def wrapper(*args, **kwargs):
-        threading.Thread(target=func, args=args, kwargs=kwargs).start()
+        threading.Thread(
+            target=func, args=args, kwargs=kwargs, daemon=True
+        ).start()
     return wrapper
