a20f0f91de4edba930e4b8e6a2650d4180012e02
Matthias Schreiber <mat@boar.bar>
2016-09-15 18:36:09 +0200

    tiles are now moving depending on camera position

--- controllers/world.py
+++ controllers/world.py
@@ -12,24 +12,29 @@ class World(FloatLayout):
     real_center = ListProperty([0, 0])
 
     def on_real_center(self, wg, pos):
-        pass
+        for tile in self.tiles.values():
+            x, y = tile.model.x, tile.model.y
+            tile.x, tile.y = self.real_to_wg(x, y)
 
     def update_tiles(self, tiles):
         for tile_model in tiles.values():
-            x, y = tile_model.x, tile_model.y
-            cx, cy = self.real_center[0], self.real_center[1]
-            off_x = (x - cx) * self.zoom
-            off_y = (y - cy) * self.zoom
-            x = self.center[0] + off_x
-            y = self.center[1] + off_y
+            x, y = self.real_to_wg(tile_model.x, tile_model.y)
 
             tile = Tile(tile_model, (x, y), self.zoom)
             pos = (tile_model.x, tile_model.y)
             try:
                 old_tile = self.tiles[pos]
-            except IndexError:
+            except KeyError:
                 pass
             else:
                 self.remove_children([old_tile])
             self.tiles[pos] = tile
             self.add_widget(tile)
+
+    def real_to_wg(self, x, y):
+        cx, cy = self.real_center[0], self.real_center[1]
+        off_x = (x - cx) * self.zoom
+        off_y = (y - cy) * self.zoom
+        x = self.center[0] + off_x
+        y = self.center[1] + off_y
+        return x, y
