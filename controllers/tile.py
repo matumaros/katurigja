bfea43e0b16f972bcd025beb89f0958426bf4bef
Matthias Schreiber <mat@boar.bar>
2016-09-16 20:06:17 +0200

    removed TilePixel and replaced it by drawing rectangles to Tile directly

--- controllers/tile.py
+++ controllers/tile.py
@@ -2,7 +2,8 @@
 
 import random
 
-from kivy.graphics import Color
+from kivy.graphics import Color, Rectangle
+from kivy.graphics.instructions import InstructionGroup
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
@@ -19,29 +20,39 @@ GROUND_TYPES = {
 Builder.load_file('views/tile.kv')
 
 
-class Tile(GridLayout):
+class Tile(Widget):
     def __init__(self, model, pos, zoom):
         super().__init__()
+        self.rectangles = []
         self.model = model
-        self.update_layout()
         self.update_pos(pos, zoom)
+        self.update_layout()
 
     def update_layout(self):
-        self.clear_widgets()
+        self.canvas.clear()
+        self.rectangles.clear()
 
-        for i in range(self.cols*self.rows):
-            color = random.choice(GROUND_TYPES[self.model.ground_type])
-            pixel = TilePixel(color)
-            self.add_widget(pixel)
+        for x in range(self.cols):
+            for y in range(self.rows):
+                instr = InstructionGroup()
+                
+                color = random.choice(GROUND_TYPES[self.model.ground_type])
+                instr.add(Color(rgb=color))
+
+                width = self.width / self.cols
+                height = self.height / self.rows
+                rectangle = Rectangle(
+                    pos=(self.pos[0]+x*width, self.pos[1]+y*height),
+                    size=(width, height),
+                )
+                instr.add(rectangle)
+                self.rectangles.append(rectangle)
+
+                self.canvas.add(instr)
 
     def update_pos(self, pos, zoom):
+        off_x, off_y = self.pos[0] - pos[0], self.pos[1] - pos[1]
+        for rect in self.rectangles:
+            rect.pos = (rect.pos[0]+off_x, rect.pos[1]+off_y)
         self.pos = pos
         self.size = (self.default_size*zoom, self.default_size*zoom)
-
-
-class TilePixel(Widget):
-    color = ListProperty([1, 1, 1, 1])
-
-    def __init__(self, color):
-        super().__init__()
-        self.color = color
